5e8caede0a3ee147dbab457a349159e8bdd96288
dEnysss <58307006+MsLolita@users.noreply.github.com>
2022-02-21 10:09:00 +0200

    Update tgRedirect.py

--- tgRedirect.py
+++ tgRedirect.py
@@ -3,19 +3,28 @@ import datetime, time
 import tgcrypto
 from pyrogram import Client, filters
 
-chatIdDonor = [-1001669121688, -1001656259847, -1001668134966]
-to = -1001547629672
+privateTrash = -1001656259847
+privateCryptoHan = -1001669121688
+floodCryptohan = -1001668134966
+
+solyanka = -1001679804682
+privateBoys = -1001547629672
 
 PHONE_NUMBER = "+6281338436161"
 
 app = Client("privateRedirect", phone_number = PHONE_NUMBER)
 
-@app.on_message(filters.chat(chatIdDonor))
+@app.on_message(filters.chat(privateTrash, privateCryptoHan))
 def takeSignal(client, message):
     time.sleep(59)
     postText = message["text"]
-    a = message.copy(to)
-    print(a["text"])
+    message.copy(privateBoys)
+    
+@app.on_message(filters.chat(floodCryptohan))
+def takeSignal(client, message):
+    time.sleep(59)
+    postText = message["text"]
+    message.copy(solyanka)
 
 if __name__ == "__main__":
     print("Start!")
